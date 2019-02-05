0f4e5451bc1ea99c247d6181e03a074f8f15554b
Maxis55 <maxis.55.m@gmail.com>
2019-02-05 12:57:09 +0200

    Including js in welcome.blade, checking vue functionality

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -84,16 +84,11 @@
                     Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div id="app">
+                    <example-component></example-component>
                 </div>
             </div>
         </div>
     </body>
+<script src="{{asset('js/app.js')}}"></script>
 </html>
